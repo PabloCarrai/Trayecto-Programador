cbd9ebc79e3270824d9b0cf79216f85c36a0928b
PabloCarrai <pablohernanjuan@hotmail.com>
2025-06-04 13:50:06 -0300

    Quedo mal la cantidad de vueltas de los bucles. Arreglado

--- 2025-Tecnicas-Programacion/Clase-20/Python/Ejercicio.py
+++ 2025-Tecnicas-Programacion/Clase-20/Python/Ejercicio.py
@@ -7,7 +7,7 @@ notas=[]
 #   Lista con los nombre de los estudiantes
 nombres=[]
 #   Creo el for para ingresar las notas
-for i in range(3):
+for i in range(10):
     nombre=input("Nombre del estudiante  ")
     nota=int(input(f"Ingrese la nota del alumno {nombre}: "))
     while(nota<0 or nota>10):
@@ -19,5 +19,5 @@ for i in range(3):
 
 #   Muestro las notas   
 print("Las notas ingresadas son ")
-for i in range(3):
+for i in range(10):
     print(f"Nota estudiante: {nombres[i]} Nota: {notas[i]}: ")
